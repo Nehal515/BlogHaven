47cdb306dac598b4ce6b1ae208ef7748148088d8
Nehal Ansari <nehalansari98065@gmail.com>
2026-01-14 15:43:57 +0545

    Genres

--- blog_app/apps.py
+++ blog_app/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.db.utils import OperationalError
 
 
 class AppConfig(AppConfig):
@@ -6,5 +7,12 @@ class AppConfig(AppConfig):
     name = 'blog_app'
 
     def ready(self):
-        from .create_superuser import create_superuser
-        create_superuser()
+        try:
+            from .models import Genre
+            genres = ["Technology", "Adventure", "Food", "Lifestyle", "Education"]
+
+            for g in genres:
+                Genre.objects.get_or_create(name=g)
+
+        except OperationalError:
+            pass
